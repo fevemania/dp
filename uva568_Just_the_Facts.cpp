87b976aa4798a57c183cea451b79dc6e5b707a6a
fevemania <brent12052003@gmail.com>
2016-12-01 21:26:56 +0800

    need to keep track with more than 1 digit

--- uva568_Just_the_Facts.cpp
+++ uva568_Just_the_Facts.cpp
@@ -16,33 +16,41 @@
 // 5   20 -> 2
 // 6   12 -> 2
 
+// second revised: We cannot just keep track of the last_non_zero, it also depend on the front digits
+// ex. when n = 14, the last few digit is 1200, and so that n = 15, the last few digit is 8000, 8 is decided by 12 * 15 = 180.
+// Because of n's upperbound is 10000, I decide the keep the last_five_digit in every DP_table
 #include <cstdio>
+const int table_size = 10005;
+const int digit_size = 10000;
+const int upperbound = 100000;
 
-
-const table_size = 10005;
-const digit_size = 10000;
-
-
+struct DP_table {
+	int last_digit;
+	int last_five_digit;
+};
+DP_table dp[table_size];
 void print_last_non_zero(int n) {
-	printf(“% 5d”, n);
-	printf(“ -> %d, dp[n]);
-	printf(“\n”);
+	printf("%5d", n);
+	printf(" -> %d", dp[n].last_digit);
+	printf("\n");
 }
 
 
 int main() {
-	int dp[table_size];
-	dp[0] = 1;
-	dp[1] = 1;
+	dp[0].last_digit = 1;
+	dp[0].last_five_digit = 1;
+	dp[1].last_digit = 1;
+	dp[1].last_five_digit = 1;
 	for (int i = 2; i <= 10000; ++i) {
-		int temp = dp[i-1] * i;
+		int temp = dp[i-1].last_five_digit * i;
 		while(!(temp%10)) {
 			temp /= 10;
 		}
-		dp[i] = temp%10;
+		dp[i].last_five_digit = temp%upperbound;
+		dp[i].last_digit = temp%10;
 	}
 	int n;
-	while(scanf(“%d”, &n) != EOF) {
+	while(scanf("%d", &n) != EOF) {
 		print_last_non_zero(n);
 	}
 }
